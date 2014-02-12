6d9a3c23f5fbcbea5c2726c016cbe9d02d750f03
Jamal Moubarak <jamalmoubarak@fullsail.edu>
2014-02-12 08:24:16 -0500

    Initial commit.  App about 80 percent done.

---
+++ wwwMedia/wwwMedia/SurveyViewController.m
@@ -0,0 +1,40 @@
+//
+//  SurveyViewController.m
+//  wwwMedia
+//
+//  Created by Jamal Moubarak on 2/12/14.
+//  Copyright (c) 2014 Jamal Moubarak. All rights reserved.
+//
+
+#import "SurveyViewController.h"
+
+@interface SurveyViewController ()
+
+@end
+
+@implementation SurveyViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [surveyWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.surveymonkey.com/s/W6QR5D8"]]];
+    
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
